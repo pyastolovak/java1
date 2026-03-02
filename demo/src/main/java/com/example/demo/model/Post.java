684d242967249fe3579ba39b490248e390084a15
pyastolovak <pyastolovaksyxa@gmail.com>
2026-03-03 00:55:31 +0500

    6.2

--- demo/src/main/java/com/example/demo/model/Post.java
+++ demo/src/main/java/com/example/demo/model/Post.java
@@ -1,14 +1,19 @@
 package com.example.demo.model;
 
+import java.util.Date;
+
 public class Post {
     private String text;
     private Integer likes;
+    private Date creationDate;
 
-    public Post(String text) {
+    public Post(String text, Date date) {
         this.text = text;
         this.likes = 0;
+        this.creationDate = date;
     }
 
     public Integer getLikes() { return this.likes; }
     public String getText() { return this.text; }
+    public Date getCreationDate() { return this.creationDate; }
 }
--- demo/src/main/java/com/example/demo/service/PostService.java
+++ demo/src/main/java/com/example/demo/service/PostService.java
@@ -4,15 +4,16 @@ import com.example.demo.model.Post;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostService {
     public List<Post> listAllPosts() {
         List<Post> posts = new ArrayList<>(List.of(
-                new Post("1"),
-                new Post("2"),
-                new Post("3")));
+                new Post("1", new Date()),
+                new Post("2", new Date()),
+                new Post("3", new Date())));
         return posts;
     }
 }
