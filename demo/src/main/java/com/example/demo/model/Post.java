526b556858d949b2efe90c97763be821a8a558b3
pyastolovak <pyastolovaksyxa@gmail.com>
2026-03-03 01:48:11 +0500

    8.5

--- demo/src/main/java/com/example/demo/model/Post.java
+++ demo/src/main/java/com/example/demo/model/Post.java
@@ -6,14 +6,19 @@ public class Post {
     private String text;
     private Integer likes;
     private Date creationDate;
+    private Long id;
 
-    public Post(String text, Date date) {
+    public Post(Long id, String text, Date date) {
         this.text = text;
         this.likes = 0;
         this.creationDate = date;
+        this.id = id;
     }
 
     public Integer getLikes() { return this.likes; }
     public String getText() { return this.text; }
     public Date getCreationDate() { return this.creationDate; }
+    public Long getId() { return this.id; }
+
+    public void setLikes(Integer likes) { this.likes = likes; }
 }
--- demo/src/main/java/com/example/demo/service/PostService.java
+++ demo/src/main/java/com/example/demo/service/PostService.java
@@ -10,15 +10,15 @@ import java.util.List;
 @Service
 public class PostService {
     List<Post> posts = new ArrayList<>(List.of(
-            new Post("1", new Date()),
-            new Post("2", new Date()),
-            new Post("3", new Date())));
+            new Post(0L, "1", new Date()),
+            new Post(1L, "2", new Date()),
+            new Post(2L, "3", new Date())));
 
     public List<Post> listAllPosts() {
         return posts;
     }
 
     public void create(String text) {
-        posts.add(new Post(text, new Date()));
+        posts.add(new Post(null, text, new Date()));
     }
 }
